40679ba1429950f5ca5752e0f88b61f9321df5bf
krzychukula <krzychukula@gmail.com>
2016-02-28 10:09:31 +0100

    Initial Commit

---
+++ nstimerTests/nstimerTests.swift
@@ -0,0 +1,36 @@
+//
+//  nstimerTests.swift
+//  nstimerTests
+//
+//  Created by Krzysztof Kula on 28/02/16.
+//  Copyright © 2016 Krzysztof Kula. All rights reserved.
+//
+
+import XCTest
+@testable import nstimer
+
+class nstimerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
