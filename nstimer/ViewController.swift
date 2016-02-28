3815abd447a907483daa8dc0344bf4d98a24eb3a
krzychukula <krzychukula@gmail.com>
2016-02-28 10:37:41 +0100

    Very simple timer

--- nstimer/ViewController.swift
+++ nstimer/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelForBinaryCount: UILabel!
+    var timer = NSTimer()
+    var binaryCount = 0b0000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateText()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +27,39 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func start(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "countUp", userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func reset(sender: AnyObject) {
+        timer.invalidate()
+        binaryCount = 0b0000
+        
+        updateText()
+    }
+    
+    func countUp() {
+        // if the counter reached max, reset it to 0
+        if(binaryCount == 0b1111) {
+            binaryCount = 0b0000
+        }else{
+            binaryCount += 0b0001
+        }
+        
+        
+        updateText()
+    }
+    
+    func updateText() {
+        var text = String(binaryCount, radix:2)
+        
+        //pad "0" to the left, to always have 4 digits
+        for _ in 0..<4 - text.characters.count {
+            text = "0" + text
+        }
+        
+        labelForBinaryCount.text = text
+    }
 }
 
